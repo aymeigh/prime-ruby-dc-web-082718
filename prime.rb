d0f317d234829de1ff9f51c923d4f59208bc8523
Amy Zhou <aymeighz@gmail.com>
2018-08-15 21:37:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(n)
  if n < 2
-   return false
-  else 
-    (2..n-1).to_a.all? do |possible_factor|
-      n % possible_factor != 0 
-    end
+  return false
+ else 
+  (2..n-1).to_a.all? do |possible_factor|
+  n % possible_factor != 0 
+  end
   end
 end
